f5befd03d135ff7966ee98c0b099b11269cc696a
Srijan Pandey <srijan.pandey2025@glbajajgroup.org>
2026-06-23 22:14:28 +0530

    day-15 uploaded

---
+++ Day-15/Question-60.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a[100], n, i, j = 0, temp;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    for(i = 0; i < n; i++) if(a[i] != 0) a[j++] = a[i];
+    while(j < n) a[j++] = 0;
+    for(i = 0; i < n; i++) printf("%d ", a[i]);
+    return 0;
+}
